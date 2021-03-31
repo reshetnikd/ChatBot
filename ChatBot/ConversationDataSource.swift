01222abd06b8165be6f4f7367db8ba9c7179a5d6
reshetnikd <dmitry.reshetnik@icloud.com>
2021-03-31 09:46:17 +0300

    Fix messageCount

--- ChatBot/ConversationDataSource.swift
+++ ChatBot/ConversationDataSource.swift
@@ -2,16 +2,18 @@ import Foundation
 class ConversationDataSource {
     
     /// The number of Messages in the conversation
-    let messageCount = 0
+    var messageCount = 0
     
     /// Add a new question to the conversation
     func add(question: String) {
         print("Asked to add question: \(question)")
+        messageCount += 1
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
         print("Asked to add answer: \(answer)")
+        messageCount += 1
     }
     
     /// The Message at a specific point in the conversation
